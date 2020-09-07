9cd0dfed1f31957c59867109f746231aaab5820b
skagawa2 <skagawa2@polar.life.illinois.edu>
2020-09-07 10:08:55 -0500

    removed symlink in .vim and edited copy_files.sh

--- .vim/.vim
+++
@@ -1,1 +0,0 @@
-/Users/shawkagawa/.vim
--- copy_files.sh
+++ copy_files.sh
@@ -4,7 +4,14 @@ cp .bashrc ~/.bashrc
 cp .git-prompt.sh ~/.git-prompt.sh
 # cp ~/.vim .vim
 cp .vimrc ~/.vimrc
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/undo
 cp .tmux.conf ~/.tmux.conf
+
+# setup tmux plugin manager
+if [[ ! -f ~/.tmux/plugins/tpm/tpm ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 if [[ -f ~/.zshrc ]]; then
   cp .zshrc ~/.zshrc
@@ -12,5 +19,7 @@ fi
 
 if [[ -f ~/.config/nvim/init.vim ]]; then
   cp init.vim ~/.config/nvim/init.vim
+else
+  echo "not using nvim?"
 fi
 
